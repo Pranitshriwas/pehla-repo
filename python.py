91ac6a3143ffb42d57073b7cd0cdc24669655dfd
Pranitshriwas <pranitshrivas142001@gmail.com>
2025-04-24 10:10:27 +0530

    updated code

--- python.py
+++ python.py
@@ -170,14 +170,14 @@ else:
 
 # mark  = int(input("enter the mark of the student :"))
 
-if mark >= 90:
-   print("grade A")
-elif mark >= 80 and mark < 90:
-   print("grade B")
-elif mark >= 70 and mark < 80:
-   print("grade C")
-else:
-   print("grade D")
+# if mark >= 90:
+#    print("grade A")
+# elif mark >= 80 and mark < 90:
+#    print("grade B")
+# elif mark >= 70 and mark < 80:
+#    print("grade C")
+# else:
+#    print("grade D")
 
 
 
